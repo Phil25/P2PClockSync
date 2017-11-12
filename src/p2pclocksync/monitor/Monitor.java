79ea76d5aedcbdbe05d466a33febb9416f9230a1
Phil25 <phil25@protonmail.com>
2017-11-12 09:21:25 +0100

    External HTML reader and refresher

--- src/p2pclocksync/monitor/Monitor.java
+++ src/p2pclocksync/monitor/Monitor.java
@@ -1,8 +1,10 @@
 package p2pclocksync.monitor;
 
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -12,27 +14,6 @@ import java.util.Scanner;
 import p2pclocksync.data.AgentData;
 
 public class Monitor{
-
-	static class RequestHandler implements HttpHandler{
-		@Override
-		public void handle(HttpExchange t) throws IOException{
-			String resp = "<head><meta http-equiv=\"refresh\" content=\"1\" /></head><h1>Sockets:</h1>";
-			resp = buildTable(resp);
-			t.sendResponseHeaders(200, resp.length());
-			OutputStream os = t.getResponseBody();
-			os.write(resp.getBytes());
-			os.close();
-		}
-
-		private String buildTable(String resp){
-			int len = data.size();
-			if(len == 0)
-				return resp += "No agents registered.";
-			for(int i = 0; i < len; i++)
-				resp += data.get(i).ip + " " + data.get(i).port + " -- " + data.get(i).clock + "ms <br />";
-			return resp;
-		}
-	}
 
 	private static HttpServer server = null;
 	private static ArrayList<AgentData> data = null;
@@ -50,9 +31,49 @@ public class Monitor{
 			System.err.println("Could not create HTTP server: " + e.getCause());
 			return;
 		}
-		server.createContext("/", new RequestHandler());
+		server.createContext("/", (HttpExchange t) -> httpHandler(t));
 		server.setExecutor(null);
 		server.start();
+	}
+
+	private static void httpHandler(HttpExchange t){
+		try{
+			String response = readFile("./src/p2pclocksync/monitor/index.html");
+			response = response.replaceAll("\\$AGENT_TABLE\\$", buildTable());
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}catch(IOException e){}
+	}
+
+	private static String readFile(String path){
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			while(line != null){
+				sb.append(line);
+				sb.append("\n");
+				line = br.readLine();
+			}
+			return sb.toString();
+		}catch(FileNotFoundException e){
+			System.err.println("Could not find " + path);
+		}catch(IOException e){
+			System.err.println("Could not read " + path + ": " + e.getCause());
+		}
+		return null;
+	}
+
+	private static String buildTable(){
+		int len = data.size();
+		if(len == 0)
+			return "No agents registered.";
+		String table = "";
+		for(int i = 0; i < len; i++)
+			table += data.get(i).ip + ':' + data.get(i).port + " -- " + data.get(i).clock + "ms <br />";
+		return table;
 	}
 
 	private static void processUserCommands(){
