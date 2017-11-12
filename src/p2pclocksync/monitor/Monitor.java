d857dc00f1b38359c063875d8ba39a9dd7adf158
Phil25 <phil25@protonmail.com>
2017-11-12 12:07:17 +0100

    Header-style website refresh

--- src/p2pclocksync/monitor/Monitor.java
+++ src/p2pclocksync/monitor/Monitor.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
@@ -15,36 +16,50 @@ import p2pclocksync.data.AgentData;
 
 public class Monitor{
 
+	private static final int PORT = 3000;
 	private static HttpServer server = null;
 	private static ArrayList<AgentData> data = null;
 
 	public static void main(String[] args){
 		data = new ArrayList<AgentData>();
-		createHttpServer();
+		if(createHttpServer())
+			System.out.println("HTTP server running on " + server.getAddress());
 		processUserCommands(); // blocking
 	}
 
-	private static void createHttpServer(){
+	private static boolean createHttpServer(){
 		try{
-			server = HttpServer.create(new InetSocketAddress(3000), 0);
+			server = HttpServer.create(new InetSocketAddress(PORT), 0);
 		}catch(IOException e){
 			System.err.println("Could not create HTTP server: " + e.getCause());
-			return;
+			return false;
 		}
 		server.createContext("/", (HttpExchange t) -> httpHandler(t));
 		server.setExecutor(null);
 		server.start();
+		return true;
 	}
 
 	private static void httpHandler(HttpExchange t){
 		try{
+			processParams(t.getRequestURI().toString().split("(\\?|\\&)"));
+
 			String response = readFile("./src/p2pclocksync/monitor/index.html");
 			response = response.replaceAll("\\$AGENT_TABLE\\$", buildTable());
+			response = response.replaceAll("\\$PORT\\$", "" + PORT);
+
 			t.sendResponseHeaders(200, response.length());
 			OutputStream os = t.getResponseBody();
 			os.write(response.getBytes());
 			os.close();
 		}catch(IOException e){}
+	}
+
+	private static void processParams(String[] params){
+		if(params.length < 2)
+			return;
+		for(int i = 1; i < params.length; i++)
+			System.out.println(params[i]);
 	}
 
 	private static String readFile(String path){
@@ -70,10 +85,10 @@ public class Monitor{
 		int len = data.size();
 		if(len == 0)
 			return "No agents registered.";
-		String table = "";
+		String table = "<table style=\"width:20em\"><tr><th>Address</th><th>Clock</th></tr>";
 		for(int i = 0; i < len; i++)
-			table += data.get(i).ip + ':' + data.get(i).port + " -- " + data.get(i).clock + "ms <br />";
-		return table;
+			table += "<tr><th>" + data.get(i).ip + ':' + data.get(i).port + "</th><th>" + data.get(i).clock + "ms</th></tr>";
+		return table + "</table>";
 	}
 
 	private static void processUserCommands(){
